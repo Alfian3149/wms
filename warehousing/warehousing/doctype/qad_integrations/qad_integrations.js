707f8114f85455f1049942adac4068f88b3a3030
Alfian3149 <alfianpical3149@gmail.com>
2026-05-21 00:54:16 +0000

    21-05-2026 R1

--- warehousing/warehousing/doctype/qad_integrations/qad_integrations.js
+++ warehousing/warehousing/doctype/qad_integrations/qad_integrations.js
@@ -3,14 +3,17 @@
 
 frappe.ui.form.on("Qad Integrations", {
   wsa_url: function(frm) {
-    if (frm.doc.wsa_url === 'qad_api_dev_url') {
-      frm.set_value("url", "http://127.0.0.1:23079/wsa/smiiwsa");
-    }
-    else if (frm.doc.wsa_url === 'qad_api_test_url') {
+    if (frm.doc.wsa_url === 'localhost') {
       frm.set_value("url", "http://127.0.0.1:24079/wsa/smiiwsa");
     }
+    else if (frm.doc.wsa_url === 'qad_api_dev_url') {
+      frm.set_value("url", "http://smii.qad:23079/wsa/smiiwsa");
+    }
+    else if (frm.doc.wsa_url === 'qad_api_test_url') {
+      frm.set_value("url", "http://smii.qad:24079/wsa/smiiwsa");
+    }
     else if (frm.doc.wsa_url === 'qad_api_prod_url') {
-      frm.set_value("url", "http://127.0.0.1:25079/wsa/smiiwsa");
+      frm.set_value("url", "http://smii.qad:25079/wsa/smiiwsa");
     }
     else {
       frm.set_value("url", "http://127.0.0.1:24079/wsa/smiiwsa");
